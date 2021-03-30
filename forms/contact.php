77250bab1b77081517041587209bf13b2bf04c6d
Shreya Gupta <creation9369778710@gmail.com>
2021-03-30 21:50:42 +0530

    Update contact.php

--- forms/contact.php
+++ forms/contact.php
@@ -1,5 +1,5 @@
 <?php
-$toEmail = "sushrutnagula@gmail.com";
+$toEmail = "collegetote@gmail.com";
 $mailHeaders = "From: " . $_POST["firstname"] ." ".$_POST["lastname"]. "<". $_POST["email"] .">\r\n";
 $details="Name : ".$_POST["firstname"]." ".$_POST["lastname"]."\n"."Email : ".$_POST["email"]."\n"."Phone no. : + ".$_POST["countryCode"]."  ".$_POST["telephone"]."\n"."Messege : ".$_POST["subject"]."\n";
 if(mail($toEmail, $mailHeaders, $details)) {
