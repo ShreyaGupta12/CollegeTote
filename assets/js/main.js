02196d6cd646bbcc3418d7655bda3dd38f3528d4
Shreya Gupta <creation9369778710@gmail.com>
2021-04-01 12:15:40 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -219,3 +219,14 @@
   });
 
 })(jQuery);
+
+
+/* Get all elements with class="close" */
+var closebtns = document.getElementsByClassName("close");
+var i;
+
+/* Loop through the elements, and hide the parent, when clicked on */
+for (i = 0; i < closebtns.length; i++) {
+  closebtns[i].addEventListener("click", function() {
+  this.parentElement.style.display = 'none';
+})};
